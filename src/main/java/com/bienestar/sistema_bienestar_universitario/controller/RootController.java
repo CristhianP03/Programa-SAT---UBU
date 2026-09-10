81d6b7e0feaf8b87b93112187a64090f9288f097
CristhianP03 <cristhianpachecoc03@gmail.com>
2026-09-09 19:42:46 -0500

    Se coloca controlador Root

---
+++ src/main/java/com/bienestar/sistema_bienestar_universitario/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.bienestar.sistema_bienestar_universitario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "redirect:/estudiante/registro";
+    }
+}
